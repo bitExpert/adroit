68ab78c3108ff4483f1b0f8eef9de1adc6b3f7e7
dropdevcoding <peter.hildenbrand@gmail.com>
2016-05-06 13:36:58 +0200

    added missing callable resolvers

---
+++ src/bitExpert/Adroit/Responder/Resolver/CallableResponderResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Adroit package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bitExpert\Adroit\Responder\Resolver;
+
+use bitExpert\Adroit\Resolver\CallableResolver;
+
+/**
+ * Implementation of an {@link \bitExpert\Adroit\Responder\Resolver\ResponderResolver} which will
+ * return the action identifier if it is callable itself e.g. when a callable class is used as identifier
+ * or a lambda function / closure
+ */
+class CallableResponderResolver extends CallableResolver implements ResponderResolver
+{
+
+}
---
+++ tests/bitExpert/Adroit/Resolver/CallableResolverUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the Adroit package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bitExpert\Adroit\Resolver;
+
+use Zend\Diactoros\Response;
+
+/**
+ * Unit test for {@link \bitExpert\Adroit\Resolver\CallableResolver}.
+ */
+class CallableResolverUnitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CallableResolver
+     */
+    protected $resolver;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->resolver = new CallableResolver();
+    }
+
+    /**
+     * @test
+     */
+    public function returnsArgumentIfCallable()
+    {
+        $identifier = function () {};
+        $result = $this->resolver->resolve($identifier);
+
+        $this->assertSame($identifier, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsNullIfArgumentIsNotCallable()
+    {
+        $identifier = 'notCallable';
+        $result = $this->resolver->resolve($identifier);
+
+        $this->assertNull($result);
+    }
+}
